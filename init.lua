99c17aad38c2bf03777f626e7bce4cac970626fa
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-08-13 07:35:37 +0600

    Add noice.nvim plugin

--- init.lua
+++ init.lua
@@ -270,6 +270,21 @@ require('lazy').setup({
   --
   { 'rcarriga/nvim-notify' },
   {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
